23483f4ea0e44b77b5ff76d14aeee5a35421d0e8
worranhin <worranhin@foxmail.com>
2024-11-06 21:01:21 +0800

    hot fix

--- JointsPosition.cs
+++ JointsPosition.cs
@@ -12,11 +12,11 @@ namespace DOF5RobotControl_GUI
 {
     internal class JointsPosition(int r1, int p2, int p3, int p4, int r5) : ObservableObject
     {
-        private int _r1;
-        private int _p2;
-        private int _p3;
-        private int _p4;
-        private int _r5;
+        private int _r1 = r1;
+        private int _p2 = p2;
+        private int _p3 = p3;
+        private int _p4 = p4;
+        private int _r5 = r5;
 
         public int R1 { get => _r1; set => SetProperty(ref _r1,value); }
         public int P2 { get => _p2; set => SetProperty(ref _p2, value); }
