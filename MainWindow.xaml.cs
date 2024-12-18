a05b408ee4511dc2709a933c01d4d1a1766a931a
worranhin <worranhin@foxmail.com>
2024-12-18 14:20:23 +0800

    添加 OPCUA 服务器 # by caoyun

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
 using System.Windows;
+using System.Windows.Forms;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -16,6 +17,11 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using DOF5RobotControl_GUI.Model;
 using DOF5RobotControl_GUI.ViewModel;
+using System.Threading;
+using Opc.Ua;
+using Opc.UaFx;
+using Opc.UaFx.Server;
+using Opc.UaFx.Services;
 using Window = System.Windows.Window;
 using Joints = DOF5RobotControl_GUI.Model.D5Robot.Joints;
 
@@ -51,9 +57,10 @@ namespace DOF5RobotControl_GUI
         readonly int natorJogResolution = 100000;
         readonly int RMDJogResolution = 20;
         readonly string natorId = "usb:id:7547982319";
-        private readonly MainViewModel viewModel = new();
-        private D5Robot? robot;
-        private JogHandler? jogHandler;
+        static private readonly MainViewModel viewModel = new();
+        static private D5Robot? robot;
+        static private JogHandler? jogHandler;
+        static public double[] joints100;
 
         public MainWindow()
         {
@@ -65,6 +72,8 @@ namespace DOF5RobotControl_GUI
                 viewModel.SelectedPort = viewModel.PortsAvailable[0];
 
             DataContext = viewModel;
+
+            joints100 = new double[5] { viewModel.TargetState.JointSpace.R1, viewModel.TargetState.JointSpace.P2, viewModel.TargetState.JointSpace.P3, viewModel.TargetState.JointSpace.P4, viewModel.TargetState.JointSpace.R5 };
         }
 
         private void PortRefresh_Click(object sender, RoutedEventArgs e)
@@ -174,6 +183,24 @@ namespace DOF5RobotControl_GUI
                 return;
             }
         }
+        public static void run2()
+        {
+            if (robot == null)
+            {
+                MessageBox.Show("Robot not connected.");
+                return;
+            }
+
+            Joints j = viewModel.TargetState.ToD5RJoints();
+            D5Robot.ErrorCode err = robot.JointsMoveAbsolute(j);
+
+            if (err != D5Robot.ErrorCode.OK)
+            {
+                MessageBox.Show($"Error while running: {err}");
+
+                return;
+            }
+        }
 
         private void BtnStop_Click(object sender, RoutedEventArgs e)
         {
@@ -207,10 +234,37 @@ namespace DOF5RobotControl_GUI
             }
         }
 
+
+        public void SomeOtherMethod(int method)
+        {
+            // 创建一个 EventArgs 实例来传递给 ButtonClicked 方法
+            EventArgs args = new EventArgs();
+            switch (method)
+            {
+                case 1: PortRefresh_Click(this, null); break;
+                case 2: BtnConnect_Click(this, null); break;
+                case 3: BtnZeroPos_Click(this, null); break;
+                case 4: BtnIdlePos_Click(this, null); break;
+                case 5: BtnPreChangeJawPos_Click(this, null); break;
+                case 6: BtnChangeJawPos_Click(this, null); break;
+                case 7: BtnAssemblePos1_Click(this, null); break;
+                case 8: BtnAssemblePos2_Click(this, null); break;
+                case 9: BtnAssemblePos3_Click(this, null); break;
+                case 10: BtnPreFetchRingPos_Click(this, null); break;
+                case 11: BtnFetchRingPos_Click(this, null); break;
+                case 12: BtnRun_Click(this, null); break;
+                case 13: BtnStop_Click(this, null); break;
+                case 14: BtnSetZero_Click(this, null); break;
+
+            }
+        }
+
+
         // R1 jogging button callbacks //
 
         private void BtnR1JogDown_N(object sender, MouseButtonEventArgs e)
         {
+            Console.WriteLine("Button was clicked!");
             if (jogHandler == null)
             {
                 MessageBox.Show("Robot not connected.");
@@ -343,6 +397,418 @@ namespace DOF5RobotControl_GUI
 
             ManualControlWindow window = new(robot);
             window.Show();
+        }
+
+        private void JointValueR1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            joints100[0] = viewModel.TargetState.JointSpace.R1;
+        }
+
+        private void JointValueP2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            joints100[1] = viewModel.TargetState.JointSpace.P2;
+        }
+
+        private void JointValueP3_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            joints100[2] = viewModel.TargetState.JointSpace.P3;
+        }
+
+        private void JointValueP4_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            joints100[3] = viewModel.TargetState.JointSpace.P4;
+        }
+
+        private void JointValueR5_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            joints100[4] = viewModel.TargetState.JointSpace.R5;
+        }
+
+        Thread serverThread;
+        private void serverRun()
+        {
+            var dof5robotInstance = new dof5robotNodeManager();
+
+            //var test = new MyNodeManager();
+            using (var server = new OpcServer("opc.tcp://localhost:4840", dof5robotInstance))//server以nodeManager初始化
+            {
+                //服务器配置
+                server.Configuration = OpcApplicationConfiguration.LoadServerConfig("Opc.UaFx.Server");
+                server.ApplicationName = "DOF5ROBOT";//应用名称
+                server.Start();
+                Random rd = new Random();
+                while (true)
+                {
+                    int i = rd.Next();
+
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+        private void BtnDisconnectServer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnConnectServer_Click(object sender, RoutedEventArgs e)
+        {
+            serverThread = new Thread(serverRun);
+            serverThread.Start();
+        }
+    }
+
+
+    public class dof5robotNodeManager : OpcNodeManager
+    {
+        private OpcDataVariableNode<double> r1_Node;
+        private OpcDataVariableNode<double> p2_Node;
+        private OpcDataVariableNode<double> p3_Node;
+        private OpcDataVariableNode<double> p4_Node;
+        private OpcDataVariableNode<double> r5_Node;
+        private OpcMethodNode method;
+
+        [return: OpcArgument("是否成功")]//说明参数
+        private bool runMotor()//运行
+        {
+            try
+            {
+                App.mainWin.SomeOtherMethod(12);
+            }
+            catch //异常处理，警告窗口
+            {
+                MessageBox.Show("CALL FAIL!");
+                return false;
+            }
+            return true;
+        }
+        private bool opcBtn_1([OpcArgument("运行", Description = "是否运行")] bool change)//刷新
+        {
+            try
+            {
+                App.mainWin.SomeOtherMethod(1);
+            }
+            catch //异常处理，警告窗口
+            {
+                MessageBox.Show("CALL FAIL!");
+                return false;
+            }
+            return true;
+        }
+        private bool opcBtn_2([OpcArgument("运行", Description = "是否运行")] bool change)//刷新
+        {
+            try
+            {
+                App.mainWin.SomeOtherMethod(2);
+            }
+            catch //异常处理，警告窗口
+            {
+                MessageBox.Show("CALL FAIL!");
+                return false;
+            }
+            return true;
+        }
+        private bool opcBtn_3([OpcArgument("运行", Description = "是否运行")] bool change)//刷新
+        {
+            try
+            {
+                App.mainWin.SomeOtherMethod(3);
+            }
+            catch //异常处理，警告窗口
+            {
+                MessageBox.Show("CALL FAIL!");
+                return false;
+            }
+            return true;
+        }
+        private bool opcBtn_4([OpcArgument("运行", Description = "是否运行")] bool change)//刷新
+        {
+            try
+            {
+                App.mainWin.SomeOtherMethod(4);
+            }
+            catch //异常处理，警告窗口
+            {
+                MessageBox.Show("CALL FAIL!");
+                return false;
+            }
+            return true;
+        }
+        private bool opcBtn_5([OpcArgument("运行", Description = "是否运行")] bool change)//刷新
+        {
+            try
+            {
+                App.mainWin.SomeOtherMethod(5);
+            }
+            catch //异常处理，警告窗口
+            {
+                MessageBox.Show("CALL FAIL!");
+                return false;
+            }
+            return true;
+        }
+        private bool opcBtn_6([OpcArgument("运行", Description = "是否运行")] bool change)//刷新
+        {
+            try
+            {
+                App.mainWin.SomeOtherMethod(6);
+            }
+            catch //异常处理，警告窗口
+            {
+                MessageBox.Show("CALL FAIL!");
+                return false;
+            }
+            return true;
+        }
+        private bool opcBtn_7([OpcArgument("运行", Description = "是否运行")] bool change)//刷新
+        {
+            try
+            {
+                App.mainWin.SomeOtherMethod(7);
+            }
+            catch //异常处理，警告窗口
+            {
+                MessageBox.Show("CALL FAIL!");
+                return false;
+            }
+            return true;
+        }
+        private bool opcBtn_8([OpcArgument("运行", Description = "是否运行")] bool change)//刷新
+        {
+            try
+            {
+                App.mainWin.SomeOtherMethod(8);
+            }
+            catch //异常处理，警告窗口
+            {
+                MessageBox.Show("CALL FAIL!");
+                return false;
+            }
+            return true;
+        }
+        private bool opcBtn_9([OpcArgument("运行", Description = "是否运行")] bool change)//刷新
+        {
+            try
+            {
+                App.mainWin.SomeOtherMethod(9);
+            }
+            catch //异常处理，警告窗口
+            {
+                MessageBox.Show("CALL FAIL!");
+                return false;
+            }
+            return true;
+        }
+        private bool opcBtn_10([OpcArgument("运行", Description = "是否运行")] bool change)//刷新
+        {
+            try
+            {
+                App.mainWin.SomeOtherMethod(10);
+            }
+            catch //异常处理，警告窗口
+            {
+                MessageBox.Show("CALL FAIL!");
+                return false;
+            }
+            return true;
+        }
+        private bool opcBtn_11([OpcArgument("运行", Description = "是否运行")] bool change)//刷新
+        {
+            try
+            {
+                App.mainWin.SomeOtherMethod(11);
+            }
+            catch //异常处理，警告窗口
+            {
+                MessageBox.Show("CALL FAIL!");
+                return false;
+            }
+            return true;
+        }
+        [return: OpcArgument("调用情况")]//此处为输出参数，类型为下函数返回的类型
+        private bool opcBtn_12([OpcArgument("运行", Description = "是否运行")] bool change)//内部为输入参数
+        {
+            try
+            {
+                App.mainWin.SomeOtherMethod(12);
+            }
+            catch //异常处理，警告窗口
+            {
+                MessageBox.Show("CALL FAIL!");
+                return false;
+            }
+            return true;
+        }
+        private bool opcBtn_13([OpcArgument("运行", Description = "是否运行")] bool change)//刷新
+        {
+            try
+            {
+                App.mainWin.SomeOtherMethod(13);
+            }
+            catch //异常处理，警告窗口
+            {
+                MessageBox.Show("CALL FAIL!");
+                return false;
+            }
+            return true;
+        }
+        private bool opcBtn_14([OpcArgument("运行", Description = "是否运行")] bool change)//刷新
+        {
+            try
+            {
+                App.mainWin.SomeOtherMethod(14);
+            }
+            catch //异常处理，警告窗口
+            {
+                MessageBox.Show("CALL FAIL!");
+                return false;
+            }
+            return true;
+        }
+
+
+        protected override IEnumerable<IOpcNode> CreateNodes(OpcNodeReferenceCollection references)
+        {
+            var rootNode = new OpcObjectNode(new OpcName("dof5", this.DefaultNamespaceIndex));
+            references.Add(rootNode, OpcObjectTypes.ObjectsFolder);
+
+            r1_Node = new OpcDataVariableNode<double>(rootNode, "r1");
+            r1_Node.Value = MainWindow.joints100[0];
+            p2_Node = new OpcDataVariableNode<double>(rootNode, "p2");
+            p2_Node.Value = MainWindow.joints100[1];
+            p3_Node = new OpcDataVariableNode<double>(rootNode, "p3");
+            p3_Node.Value = MainWindow.joints100[2];
+            p4_Node = new OpcDataVariableNode<double>(rootNode, "p4");
+            p4_Node.Value = MainWindow.joints100[3];
+            r5_Node = new OpcDataVariableNode<double>(rootNode, "r5");
+            r5_Node.Value = MainWindow.joints100[4];
+            r1_Node.WriteVariableValueCallback = handleWriteR1_NodeCallback;
+            r1_Node.ReadVariableValueCallback = handleReadR1_NodeCallback;
+            p2_Node.WriteVariableValueCallback = handleWriteP2_NodeCallback;
+            p2_Node.ReadVariableValueCallback = handleReadP2_NodeCallback;
+            p3_Node.WriteVariableValueCallback = handleWriteP3_NodeCallback;
+            p3_Node.ReadVariableValueCallback = handleReadP3_NodeCallback;
+            p4_Node.WriteVariableValueCallback = handleWriteP4_NodeCallback;
+            p4_Node.ReadVariableValueCallback = handleReadP4_NodeCallback;
+            r5_Node.WriteVariableValueCallback = handleWriteR5_NodeCallback;
+            r5_Node.ReadVariableValueCallback = handleReadR5_NodeCallback;
+            var globalControlNode = new OpcFolderNode(rootNode, "全局控制");
+            var refreshNode = new OpcMethodNode(globalControlNode, "刷新", new Func<bool, bool>(this.opcBtn_1));
+            var connectNode = new OpcMethodNode(globalControlNode, "连接", new Func<bool, bool>(this.opcBtn_2));
+            var ZeroPosNode = new OpcMethodNode(globalControlNode, "零点位", new Func<bool, bool>(this.opcBtn_3));
+            var IdlePosNode = new OpcMethodNode(globalControlNode, "待机位", new Func<bool, bool>(this.opcBtn_4));
+            var PreChangeJawPosNode = new OpcMethodNode(globalControlNode, "换夹钳预备位", new Func<bool, bool>(this.opcBtn_5));
+            var ChangeJawPosNode = new OpcMethodNode(globalControlNode, "换夹钳位", new Func<bool, bool>(this.opcBtn_6));
+            var AssemblePos1Node = new OpcMethodNode(globalControlNode, "装配位1", new Func<bool, bool>(this.opcBtn_7));
+            var AssemblePos2Node = new OpcMethodNode(globalControlNode, "装配位2", new Func<bool, bool>(this.opcBtn_8));
+            var AssemblePos3Node = new OpcMethodNode(globalControlNode, "装配位3", new Func<bool, bool>(this.opcBtn_9));
+            var PreFetchRingPosNode = new OpcMethodNode(globalControlNode, "取零件预备位", new Func<bool, bool>(this.opcBtn_10));
+            var FetchRingPosNode = new OpcMethodNode(globalControlNode, "取零件位", new Func<bool, bool>(this.opcBtn_11));
+            var runNode = new OpcMethodNode(globalControlNode, "运行", new Func<bool, bool>(this.opcBtn_12));
+            var StopNode = new OpcMethodNode(globalControlNode, "停止", new Func<bool, bool>(this.opcBtn_13));
+            var SetZeroNode = new OpcMethodNode(globalControlNode, "置零", new Func<bool, bool>(this.opcBtn_14));
+
+
+            return new IOpcNode[] { rootNode };
+
+        }
+
+
+        OpcVariableValue<object> handleWriteR1_NodeCallback(OpcWriteVariableValueContext context, OpcVariableValue<object> value)
+        {
+            object objectValue = value.Value;
+            double nodeValue = (double)objectValue;
+            {
+                // 安全地更新标签文本
+                App.mainWin.Dispatcher.Invoke(() =>
+                {
+                    App.mainWin.JointValueR1.Text = nodeValue.ToString();
+                });
+            }
+            return value;
+        }
+        OpcVariableValue<object> handleReadR1_NodeCallback(OpcReadVariableValueContext context, OpcVariableValue<object> value)
+        {
+            double r1_change = MainWindow.joints100[0];
+            OpcVariableValue<object> r = new OpcVariableValue<object>(r1_change);
+            return r;
+        }
+        OpcVariableValue<object> handleWriteP2_NodeCallback(OpcWriteVariableValueContext context, OpcVariableValue<object> value)
+        {
+            object objectValue = value.Value;
+            double nodeValue = (double)objectValue;
+            {
+                App.mainWin.Dispatcher.Invoke(() =>
+                {
+                    App.mainWin.JointValueP2.Text = nodeValue.ToString();
+                });
+            }
+            return value;
+        }
+        OpcVariableValue<object> handleReadP2_NodeCallback(OpcReadVariableValueContext context, OpcVariableValue<object> value)
+        {
+            double r1_change = MainWindow.joints100[1];
+            OpcVariableValue<object> r = new OpcVariableValue<object>(r1_change);
+            return r;
+        }
+        OpcVariableValue<object> handleWriteP3_NodeCallback(OpcWriteVariableValueContext context, OpcVariableValue<object> value)
+        {
+            object objectValue = value.Value;
+            double nodeValue = (double)objectValue;
+            {
+                App.mainWin.Dispatcher.Invoke(() =>
+                {
+                    App.mainWin.JointValueP3.Text = nodeValue.ToString();
+                });
+            }
+            return value;
+        }
+        OpcVariableValue<object> handleReadP3_NodeCallback(OpcReadVariableValueContext context, OpcVariableValue<object> value)
+        {
+            double r1_change = MainWindow.joints100[2];
+            OpcVariableValue<object> r = new OpcVariableValue<object>(r1_change);
+            return r;
+        }
+        OpcVariableValue<object> handleWriteP4_NodeCallback(OpcWriteVariableValueContext context, OpcVariableValue<object> value)
+        {
+            object objectValue = value.Value;
+            double nodeValue = (double)objectValue;
+            {
+                App.mainWin.Dispatcher.Invoke(() =>
+                {
+                    App.mainWin.JointValueP4.Text = nodeValue.ToString();
+                });
+            }
+            return value;
+        }
+        OpcVariableValue<object> handleReadP4_NodeCallback(OpcReadVariableValueContext context, OpcVariableValue<object> value)
+        {
+            double r1_change = MainWindow.joints100[3];
+            OpcVariableValue<object> r = new OpcVariableValue<object>(r1_change);
+            return r;
+        }
+        OpcVariableValue<object> handleWriteR5_NodeCallback(OpcWriteVariableValueContext context, OpcVariableValue<object> value)
+        {
+            object objectValue = value.Value;
+            double nodeValue = (double)objectValue;
+            {
+                App.mainWin.Dispatcher.Invoke(() =>
+                {
+                    App.mainWin.JointValueR5.Text = nodeValue.ToString();
+                });
+            }
+            return value;
+        }
+        OpcVariableValue<object> handleReadR5_NodeCallback(OpcReadVariableValueContext context, OpcVariableValue<object> value)
+        {
+            double r1_change = MainWindow.joints100[4];
+            OpcVariableValue<object> r = new OpcVariableValue<object>(r1_change);
+            return r;
+        }
+        public dof5robotNodeManager()
+        : base("dof5")
+        {
+
         }
     }
 }
