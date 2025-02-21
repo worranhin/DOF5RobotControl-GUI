873c04d999a391c548b76a9181f1dfc2a2461635
worranhin <worranhin@foxmail.com>
2025-02-21 18:28:42 +0800

    添加相机电机控制服务类

---
+++ D5RobotWPF/Services/ICamMotorControlService.cs
@@ -0,0 +1,12 @@
+﻿namespace DOF5RobotControl_GUI.Services
+{
+    public interface ICamMotorControlService
+    {
+        void Connect(string port);
+        void Disconnect();
+        void MoveRelativeLeft(CamMotorControlService.MotorSelect id, int data);
+        void MoveRelativeRight(CamMotorControlService.MotorSelect id, int data);
+        void MoveStepLeft(CamMotorControlService.MotorSelect id);
+        void MoveStepRight(CamMotorControlService.MotorSelect id);
+    }
+}
