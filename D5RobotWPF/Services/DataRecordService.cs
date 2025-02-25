7ce17c668ef6accf8b91ffaab9fb9f79764fab6d
worranhin <worranhin@foxmail.com>
2025-02-25 22:34:30 +0800

    创建 DataRecord 服务，用于收集数据

--- D5RobotWPF/Services/DataRecordService.cs
+++ D5RobotWPF/Services/DataRecordService.cs
@@ -1,11 +1,8 @@
 ﻿using DOF5RobotControl_GUI.Model;
-using System;
-using System.Collections.Generic;
+using OpenCvSharp;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace DOF5RobotControl_GUI.Services
 {
@@ -14,15 +11,34 @@ namespace DOF5RobotControl_GUI.Services
 
     public class DataRecordService : IDataRecordService
     {
+        private List<StateRecord> records = [];
+
         public void Record(JointSpace joints, CamFrame topFrame, CamFrame bottomFrame)
         {
             DateTimeOffset currentTime = DateTimeOffset.UtcNow;
             long timestamp_ms = currentTime.ToUnixTimeMilliseconds();
-            string topImgStr = "TopImg_" + timestamp_ms.ToString();
-            string bottomImgStr = "BottomImg_" + timestamp_ms.ToString();
+            string topImgStr = "TopImg_" + timestamp_ms.ToString() + ".bmp";
+            string bottomImgStr = "BottomImg_" + timestamp_ms.ToString() + ".bmp";
             StateRecord record = new(timestamp_ms, joints.R1, joints.P2, joints.P3, joints.P4, joints.R5, topImgStr, bottomImgStr);
-            string jsonStr = JsonSerializer.Serialize(record);
+            records.Add(record);
+            string jsonStr = JsonSerializer.Serialize(records);
             Debug.WriteLine(jsonStr);
+
+            string rootDir = "Records_" + timestamp_ms.ToString();
+            Directory.CreateDirectory(rootDir);
+            var jsonPath = Path.Combine(rootDir, "data.json");
+            using (StreamWriter sw = File.CreateText(jsonPath))
+            {
+                sw.WriteLine(jsonStr);
+            }
+
+            string imageDir = Path.Combine(rootDir, "Images");
+            Directory.CreateDirectory(imageDir);
+
+            byte[] imageBytes = topFrame.Buffer;
+            string imagePath = Path.Combine(imageDir, topImgStr);
+            var img = Mat.FromPixelData(topFrame.Height, topFrame.Width, MatType.CV_8UC1, imageBytes);
+            img.ImWrite(imagePath);
         }
     }
 }
