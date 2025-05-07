8427742fb16459c91d9ef2ab6fa45811a1f58647
worranhin <worranhin@foxmail.com>
2025-05-07 14:17:00 +0800

    通过提供设计模型修复 XAML Designer 的错误

---
+++ D5RobotWPF/DesignModel/DCameraControlService.cs
@@ -0,0 +1,67 @@
+﻿using DOF5RobotControl_GUI.Model;
+using DOF5RobotControl_GUI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOF5RobotControl_GUI.DesignModel
+{
+    class DCameraControlService : ICameraControlService
+    {
+        public bool CameraIsOpened => false;
+
+        public bool CamMotorIsConnected => false;
+
+        public void CloseCamera()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ConnectCamMotor(string port)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisconnectCamMotor()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CamFrame GetBottomFrame()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CamFrame GetTopFrame()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveBottomCamera(int angle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveTopCamera(int distance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OpenCamera()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterCallback(EventHandler<CamFrame> TopFrameReceivedHandler, EventHandler<CamFrame> BottomFrameReceivedHandler)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnRegisterCallback(EventHandler<CamFrame> TopFrameReceivedHandler, EventHandler<CamFrame> BottomFrameReceivedHandler)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ D5RobotWPF/DesignModel/DRobotControlService.cs
@@ -0,0 +1,74 @@
+﻿using DOF5RobotControl_GUI.Model;
+using DOF5RobotControl_GUI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOF5RobotControl_GUI.DesignModel
+{
+    class DRobotControlService : IRobotControlService
+    {
+        public bool RobotIsConnected => false;
+
+        public RoboticState CurrentState { get; } = new();
+
+        public RoboticState TargetState { get; } = new();
+
+        public void Connect(string port)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Disconnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public RoboticState GetCurrentState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveRelative(RoboticState relative)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveTo(RoboticState target)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetZero()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartVibrate(bool vibrateHorizontal, bool vibrateVertical, double amplitude, double frequency)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopVibrate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task WaitForTargetedAsync(int CheckPeriod = 100, double CheckDistance = 0.1)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task WaitForTargetedAsync(CancellationToken token, int CheckPeriod = 100, double CheckDistance = 0.1)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
